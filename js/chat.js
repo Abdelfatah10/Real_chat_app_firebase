71a1aefbe78679ea365561840e29cde3cfb1e97b
Abdelfatah10 <ab.djaballah@univ-batna2.dz>
2026-03-14 19:29:45 +0100

    real chat app

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, get, push, set, onValue } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-database.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
@@ -17,6 +18,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getDatabase();
 const auth = getAuth(app);
 
