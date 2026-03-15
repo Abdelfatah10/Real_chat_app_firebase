232db91d88b8a4e305ecb81d1be21a1dfe511a2b
Abdelfatah10 <ab.djaballah@univ-batna2.dz>
2026-03-15 12:44:30 +0100

    analytics

--- js/chat.js
+++ js/chat.js
@@ -1,7 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
-import { getDatabase, ref, get, push, set, onValue } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-database.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import { getDatabase, ref, get, push, set, onValue } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
 // ============================================
 // FIREBASE CONFIG
